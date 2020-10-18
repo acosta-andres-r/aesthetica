9d5e251efc0ad7696c160dc428c4447e64096eeb
Luna <dlramos194@gmail.com>
2020-10-18 18:11:45 -0500

    women's apparel button

--- public/js/w_shop.js
+++ public/js/w_shop.js
@@ -1,47 +1,69 @@
-// Post to retrieve Pexel image URLs
-$("#explore").on("click", function(){
+//Buttons for user searches :
+
+$("#women's").on("click", function() {
     $.post("/api/explorer", {
-        search: "fashion"
-      })
+            search: "women's apparel"
+        })
         .then((res) => {
-          console.log(res);
-          // If there's an error, log the error
+            console.log(res);
+            // If there's an error, log the error
         })
         .catch(err => {
-          console.log(err);
+            console.log(err);
         });
 })
 
-// Post to save an image
-$("#favorite").on("click", function(){
 
-    $.post("/api/images", {
-      imageURL: "https://images.pexels.com/photos/3317434/pexels-photo-3317434.jpeg?auto=compress&cs=tinysrgb&h=350",
-      public_id: "3317434",
-      UserId: $("#welcome").data("user-id")
-    })
-        .then((res) => {
-          console.log(res);
-          // If there's an error, log the error
-        })
-        .catch(err => {
-          console.log(err);
-        });
+
+
+// Post to retrieve Pexel image URLs
+$("#explore").on("click", function() {
+$.post("/api/explorer", {
+    search: "fashion"
+})
+.then((res) => {
+    console.log(res);
+    // If there's an error, log the error
+})
+.catch(err => {
+    console.log(err);
+});
+})
+
+
+
+
+
+// Post to save an image
+// $("#favorite").on("click", function(){
+
+//     $.post("/api/images", {
+//       imageURL: "https://images.pexels.com/photos/3317434/pexels-photo-3317434.jpeg?auto=compress&cs=tinysrgb&h=350",
+//       public_id: "3317434",
+//       UserId: $("#welcome").data("user-id")
+//     })
+//         .then((res) => {
+//           console.log(res);
+//           // If there's an error, log the error
+//         })
+//         .catch(err => {
+//           console.log(err);
+//         });
 })
 
 // Post to delete an image 
-$("#unfavorite").on("click", function(){
+// $("#unfavorite").on("click", function(){
 
-  $.ajax({
-    method: "DELETE",
-    url: "/api/images/",
-    dataType: 'json',
-    data: {
-      public_id: "3317434",
-      UserId: $("#welcome").data("user-id")
-    }
-  })
-    .then(function(response) {
-      console.log(response);
-    });
+//   $.ajax({
+//     method: "DELETE",
+//     url: "/api/images/",
+//     dataType: 'json',
+//     data: {
+//       public_id: "3317434",
+//       UserId: $("#welcome").data("user-id")
+//     }
+//   })
+//     .then(function(response) {
+//       console.log(response);
+//     });
 })
