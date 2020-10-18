b0c043608b2017f13afc79655108ee7b9414d5f3
Andres A <acosta.andres.r@gmail.com>
2020-10-18 15:25:39 -0500

    Incorporate event listener for favorite (save) and unfavorite (delete) buttons.

--- public/js/w_shop.js
+++ public/js/w_shop.js
@@ -1,5 +1,5 @@
-// Post to /api/explorer 
-$("button").on("click", function(){
+// Post to retrieve Pexel image URLs
+$("#explore").on("click", function(){
     $.post("/api/explorer", {
         search: "fashion"
       })
@@ -10,4 +10,38 @@ $("button").on("click", function(){
         .catch(err => {
           console.log(err);
         });
+})
+
+// Post to save an image
+$("#favorite").on("click", function(){
+
+    $.post("/api/images", {
+      imageURL: "https://images.pexels.com/photos/3317434/pexels-photo-3317434.jpeg?auto=compress&cs=tinysrgb&h=350",
+      public_id: "3317434",
+      UserId: $("#welcome").data("user-id")
+    })
+        .then((res) => {
+          console.log(res);
+          // If there's an error, log the error
+        })
+        .catch(err => {
+          console.log(err);
+        });
+})
+
+// Post to delete an image 
+$("#unfavorite").on("click", function(){
+
+  $.ajax({
+    method: "DELETE",
+    url: "/api/images/",
+    dataType: 'json',
+    data: {
+      public_id: "3317434",
+      UserId: $("#welcome").data("user-id")
+    }
+  })
+    .then(function(response) {
+      console.log(response);
+    });
 })
