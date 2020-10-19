753116415a97b69bff96dd3512813f411b625dea
Zack White <zhwhite18@yahoo.com>
2020-10-19 12:37:21 -0500

    commit

--- public/js/w_shop.js
+++ public/js/w_shop.js
@@ -1,36 +1,36 @@
 // Post to retrieve Pexel image URLs
-$("#explore").on("click", function(){
-    $.post("/api/explorer", {
-        search: "fashion"
-      })
-        .then((res) => {
-          console.log(res);
-          // If there's an error, log the error
-        })
-        .catch(err => {
-          console.log(err);
-        });
+$("#explore").on("click", function () {
+  $.post("/api/explorer", {
+    search: "fashion"
+  })
+    .then((res) => {
+      console.log(res);
+      // If there's an error, log the error
+    })
+    .catch(err => {
+      console.log(err);
+    });
 })
 
 // Post to save an image
-$("#favorite").on("click", function(){
+$("#favorite").on("click", function () {
 
-    $.post("/api/images", {
-      imageURL: "https://images.pexels.com/photos/3317434/pexels-photo-3317434.jpeg?auto=compress&cs=tinysrgb&h=350",
-      public_id: "3317434",
-      UserId: $("#welcome").data("user-id")
+  $.post("/api/images", {
+    imageURL: "https://images.pexels.com/photos/3317434/pexels-photo-3317434.jpeg?auto=compress&cs=tinysrgb&h=350",
+    public_id: "3317434",
+    UserId: $("#welcome").data("user-id")
+  })
+    .then((res) => {
+      console.log(res);
+      // If there's an error, log the error
     })
-        .then((res) => {
-          console.log(res);
-          // If there's an error, log the error
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    .catch(err => {
+      console.log(err);
+    });
 })
 
 // Post to delete an image 
-$("#unfavorite").on("click", function(){
+$("#unfavorite").on("click", function () {
 
   $.ajax({
     method: "DELETE",
@@ -41,18 +41,18 @@ $("#unfavorite").on("click", function(){
       UserId: $("#welcome").data("user-id")
     }
   })
-    .then(function(response) {
+    .then(function (response) {
       console.log(response);
     });
 })
 
-$("#logout").on("click", function() {
+$("#logout").on("click", function () {
   $.get("/logout")
-  .then(() => {
-    window.location.replace("/");
-    // If there's an error, log the error
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(() => {
+      window.location.replace("/");
+      // If there's an error, log the error
+    })
+    .catch(err => {
+      console.log(err);
+    });
 })
